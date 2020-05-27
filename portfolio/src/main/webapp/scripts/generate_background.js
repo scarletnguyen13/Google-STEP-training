91ba05364a8d8fc01912d1e0e0f09b01ec10f8b0
scarletnguyen13 <scarlet.nguyen01@gmail.com>
2020-05-27 17:40:03 -0400

    Generate background for homepage

---
+++ portfolio/src/main/webapp/scripts/generate_background.js
@@ -0,0 +1,48 @@
+import { BACKGROUND_RANDOM_COLORS, NUMBER_OF_BACKGROUND_BALLS } from './constants.js';
+
+const balls = [];
+
+const generateRandomStyle = () => {
+  const randomColorIndex = Math.floor(Math.random() * BACKGROUND_RANDOM_COLORS.length);
+  const randomSize = `${Math.random()}em`;
+
+  return {
+    background: BACKGROUND_RANDOM_COLORS[randomColorIndex],
+    left: `${Math.floor(Math.random() * 100)}vw`,
+    top: `${Math.floor(Math.random() * 90)}vh`,
+    transform: `scale(${Math.random()})`,
+    width: randomSize,
+    height: randomSize,
+  }
+}
+
+for (let i = 0; i < NUMBER_OF_BACKGROUND_BALLS; i++) {
+  const ball = document.createElement("div");
+  ball.classList.add("ball");
+  Object.assign(ball.style, generateRandomStyle())
+  
+  balls.push(ball);
+  document.getElementById('content').append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  const nextCoordinate = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${nextCoordinate.x}rem, ${nextCoordinate.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
