98ea3818bba0c2aba17cc7e4222102849782aec6
scarletnguyen13 <scarlet.nguyen01@gmail.com>
2020-06-07 02:17:55 +0000

    Successfully connected to client!

--- socket/server.js
+++ socket/server.js
@@ -1,28 +1,33 @@
 "use strict"; 
 const express = require('express'); 
-const app = express(); 
+const http = require('http');
+const socketIo = require("socket.io");
 
-app.get('/socket', (req, res) => {    
-   res.status(200).send('Hello world!');
-});
-
-//Setting up express and adding socketIo middleware
+// Setting up express and adding socketIo middleware
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-//Setting up a socket with the namespace "connection" for new sockets
-io.on("connection", socket => {
+const port = process.env.PORT || 8081;
+
+let interval;
+
+io.on("connection", (socket) => {
   console.log("New client connected");
-
-  //Here we listen on a new namespace called "incoming data"
-  socket.on("incoming data", (data) => {
-      //Here we broadcast it out to all other sockets EXCLUDING the socket which sent us the data
-      socket.broadcast.emit("outgoing data", {num: data});
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
   });
-
-  //A special namespace "disconnect" for when a client disconnects
-  socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-app.listen(process.env.PORT);
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
